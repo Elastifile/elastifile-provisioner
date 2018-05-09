546d150c6d357865c356b447a79f81633ba273b1
adam.gluck@elastifile.com <adam.gluck@elastifile.com>
2018-05-09 22:49:02 +0300

    dont do update

--- vendor/github.com/kubernetes-incubator/nfs-provisioner/controller/leaderelection/resourcelock/provisionpvclock.go
+++ vendor/github.com/kubernetes-incubator/nfs-provisioner/controller/leaderelection/resourcelock/provisionpvclock.go
@@ -23,6 +23,7 @@ import (
 
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
+	"github.com/golang/glog"
 )
 
 type ProvisionPVCLock struct {
@@ -67,8 +68,9 @@ func (pl *ProvisionPVCLock) Update(ler LeaderElectionRecord) error {
 	if err != nil {
 		return err
 	}
+	glog.Infof("AAAAAAAAAAAAAAAAAAA  recordBytes: %v" , recordBytes )
 	pl.p.Annotations[LeaderElectionRecordAnnotationKey] = string(recordBytes)
-	pl.p, err = pl.Client.Core().PersistentVolumeClaims(pl.PVCMeta.Namespace).Update(pl.p)
+	//pl.p, err = pl.Client.Core().PersistentVolumeClaims(pl.PVCMeta.Namespace).Update(pl.p)
 	return err
 }
 

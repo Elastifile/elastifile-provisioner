96a2210d276875e38eb68c582909d388101c6c69
Liran Mimony <liran.mimony@elastifile.com>
2018-05-13 19:56:50 +0300

    wip

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
-	"elastifile-provisioner/elastifile"
+	"github.com/elastifile/elastifile-provisioner/elastifile"
 	"fmt"
 )
 

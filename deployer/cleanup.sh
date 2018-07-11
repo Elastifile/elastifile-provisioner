36ec6551778458ae077ffe858468043f75038b12
Jean Spector <jean.spector@elastifile.com>
2018-07-11 22:06:57 +0300

    wip - cleanup

--- deployer/cleanup.sh
+++ deployer/cleanup.sh
@@ -3,5 +3,8 @@ kubectl delete deployment elastifile-provisioner-1-elastifile-provisioner
 kubectl delete job.batch/elastifile-provisioner-1-deployer
 kubectl delete job elastifile-provisioner-1-deployer-custom-script
 kubectl delete serviceaccount elastifile-provisioner-sa
+kubectl delete storageclass elastifile
 kubectl delete configmap elastifile-provisioner-1-deployer-config
+kubectl delete secret elastifile-rest
+#kubectl delete application elastifile-provisioner-1 # hangs
 set +x

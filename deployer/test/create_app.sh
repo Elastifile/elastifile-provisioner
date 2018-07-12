6be6d990552f31bc2ec1afbee1717dd449637c1f
Jean Spector <jean.spector@elastifile.com>
2018-07-12 22:04:24 +0300

    Deployer - Added secret garbage collection

--- deployer/test/create_app.sh
+++ deployer/test/create_app.sh
@@ -1,8 +1,7 @@
-EMS_URL="https://EEE"
-NFS_ADDR="n.n.n.n"
-
+PROJECT=launcher-poc-207208
 APP_TOOLS_DIR=/home/jeans/Documents/workspace/gke-repos/marketplace-k8s-app-tools-0.5
 
 set -x
-$APP_TOOLS_DIR/scripts/start.sh --deployer=gcr.io/launcher-poc-207208/elastifile-provisioner-deployer --parameters='{"name":"elastifile-provisioner-1","emanageAddress":"EMS_URL","emanagePassword":"changeme","emanageUser":"admin","imageProvisioner":"docker.io/elastifileio/provisioner:defaultstorageclass","namespace":"default","nfsAddress":"NFS_ADDR"}'
+$APP_TOOLS_DIR/scripts/start.sh --deployer=gcr.io/$PROJECT/elastifile-provisioner-deployer --parameters='{"name":"elastifile-provisioner-1","emanageAddress":"https://CHANGEME","emanagePassword":"changeme","emanageUser":"admin","imageProvisioner":"docker.io/elastifileio/provisioner:defaultstorageclass","namespace":"default","nfsAddress":"CHANGEME"}'
 set +x
+

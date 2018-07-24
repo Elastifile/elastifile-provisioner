79b4d9cd022761db5bd3af797c4525a87d3e8a26
Jean Spector <jean.spector@elastifile.com>
2018-07-24 12:59:40 +0300

    Deployer - Added PROJECT auto-detect in provisioner-pull-push.sh

--- deployer/provisioner-pull-push.sh
+++ deployer/provisioner-pull-push.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+# This script can be used to pull an updated version of the provisioner docker image
+# and push it to the Marketplace staging project
+
 TAG=1.0
 IMAGE_NAME=provisioner
+PROJECT=$(gcloud config get-value project)
 SRC_REPO=docker.io/elastifileio
-DST_REPO=gcr.io/launcher-poc-207208
+DST_REPO=gcr.io/$PROJECT
 
 docker pull $SRC_REPO/$IMAGE_NAME:$TAG
 docker tag $SRC_REPO/$IMAGE_NAME:$TAG $DST_REPO/$IMAGE_NAME:$TAG

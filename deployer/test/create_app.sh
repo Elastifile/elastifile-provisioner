bbd0e0384d557d4ef1e0ed355568f12d338c739f
Jean Spector <jean.spector@elastifile.com>
2018-07-24 13:23:25 +0300

    Deployer - Parametrized create_app.sh

--- deployer/test/create_app.sh
+++ deployer/test/create_app.sh
@@ -1,14 +1,57 @@
 #!/usr/bin/env bash
 
+MYNAME=$(basename $0)
 MYPATH=$(dirname $0)
 
+. ${MYPATH}/../functions.sh
+. ${MYPATH}/../validators.sh
+
+# Defaults
 APP_TOOLS_DIR=/home/jeans/Documents/workspace/gke-repos/marketplace-k8s-app-tools
 PROJECT=$(gcloud config get-value project)
 DOCKER_REPO=gcr.io/${PROJECT}
-DEPLOYER_IMAGE=${DOCKER_REPO}/provisioner/deployer:1.0
+TAG=1.0
+EMS_ADDR='https://FAKE_EMS_URL'
+NFS_ADDR='FAKE_NFS_ADDR'
+NAMESPACE='default'
+SERVICE_ACCOUNT='default'
+EMS_USER='admin'
+EMS_PASS='changeme'
+APP_NAME='test-prov-1'
+
+OPTS=$(getopt -o e:n:m:t:s:a:u:p: -n ${MYNAME} -- "$@")
+if [ $? != 0 ] ; then
+    logme "ERROR: Failed parsing command line arguments"
+    exit 2
+fi
+
+eval set -- "${OPTS}"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -e) EMS_ADDR=$2
+        shift 2 ;;
+    -n) NFS_ADDR=$2
+        shift 2 ;;
+    -m) APP_TOOLS_DIR=$2
+        shift 2 ;;
+    -t) TAG=$2
+        shift 2 ;;
+    -s) NAMESPACE=$2
+        shift 2 ;;
+    -a) SERVICE_ACCOUNT=$2
+        shift 2 ;;
+    -u) EMS_USER="$2"
+        shift 2 ;;
+    -p) EMS_PASS="$2"
+        shift 2 ;;
+    *) break ;;
+  esac
+done
+
+DEPLOYER_IMAGE=${DOCKER_REPO}/provisioner/deployer:${TAG}
 
 IFS='' read -r -d '' PARAMETERS <<EOF
-{"name":"ecfs-provisioner-via-start-1","emanageAddress":"https://CHANGEME","emanagePassword":"changeme","emanageUser":"admin","imageProvisioner":"${DOCKER_REPO}/provisioner:1.0","imageCustomScript":"${DOCKER_REPO}/provisioner/custom_script:1.0","namespace":"default","nfsAddress":"CHANGEME","serviceAccount":"default"}
+{"name":"${APP_NAME}","emanageAddress":"${EMS_ADDR}","emanagePassword":"${EMS_PASS}","emanageUser":"${EMS_USER}","imageProvisioner":"${DOCKER_REPO}/provisioner:${TAG}","imageCustomScript":"${DOCKER_REPO}/provisioner/custom_script:${TAG}","namespace":"${NAMESPACE}","nfsAddress":"${NFS_ADDR}","serviceAccount":"${SERVICE_ACCOUNT}"}
 EOF
 
 set -x

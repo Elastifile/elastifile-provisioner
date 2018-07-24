f5c838a7ccfeff597c044cf1c2b6cd317281fbe7
Jean Spector <jean.spector@elastifile.com>
2018-07-24 13:32:06 +0300

    Deployer - Parametrized make.sh

--- deployer/debug/make.sh
+++ deployer/debug/make.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
+MYNAME=$(basename $0)
 MYPATH=$(dirname $0)
-source $MYPATH/../functions.sh
+source ${MYPATH}/../functions.sh
 
-# TODO: Make customizable values actually customaizable
-
-# Customizable values
-if [ -n "$1" ]; then
-    PROJECT="$1"
-else
-    PROJECT=$(gcloud config get-value project)
-fi
-
-if [ -n "$2" ]; then
-    TAG="$2"
-else
-    TAG=latest
-fi
-
+# Defaults
+PROJECT=$(gcloud config get-value project)
+TAG=latest
+IMAGE=nfs-debug
 DOCKER_FILE=Dockerfile
 
-# Static values
-IMAGE=nfs-debug
+# Command-line arguments
+OPTS=$(getopt -o p:t:i:d: -n ${MYNAME} -- "$@")
+if [ $? != 0 ] ; then
+    logme "ERROR: Failed parsing command line arguments"
+    exit 2
+fi
+
+eval set -- "${OPTS}"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -p) PROJECT="$2"
+        shift 2 ;;
+    -t) TAG="$2"
+        shift 2 ;;
+    -i) IMAGE="$2"
+        shift 2 ;;
+    -d) DOCKER_FILE="$2"
+        shift 2 ;;
+    *) break ;;
+  esac
+done
 
 DOCKER_REPO=gcr.io/${PROJECT}
-IMAGE_TAGGED=${DOCKER_REPO}/$IMAGE:$TAG
-logme "Building "$DOCKER_FILE" and tagging the image as $IMAGE_TAGGED"
-exec_cmd docker build -t $IMAGE_TAGGED . -f $DOCKER_FILE
-logme Pushing $IMAGE_TAGGED
-exec_cmd docker push $IMAGE_TAGGED
+IMAGE_TAGGED=${DOCKER_REPO}/${IMAGE}:${TAG}
+logme "Building "${DOCKER_FILE}" and tagging the image as ${IMAGE_TAGGED}"
+assert_exec_cmd docker build -t ${IMAGE_TAGGED} . -f ${DOCKER_FILE}
+logme Pushing ${IMAGE_TAGGED}
+assert_exec_cmd docker push ${IMAGE_TAGGED}
 logme Listing available images
-exec_cmd gcloud container images list --repository=${DOCKER_REPO}
+assert_exec_cmd gcloud container images list --repository=${DOCKER_REPO}
 
 echo In order to free up the disk space:
-echo gcloud container images delete $IMAGE_TAGGED
-
+echo gcloud container images delete ${IMAGE_TAGGED}
--- deployer/make.sh
+++ deployer/make.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
-source functions.sh
+MYNAME=$(basename $0)
+MYPATH=$(dirname $0)
 
-if [ -n "$1" ]; then
-    PROJECT="$1"
-else
-    PROJECT=$(gcloud config get-value project)
+source ${MYPATH}/functions.sh
+
+# Defaults
+PROJECT=$(gcloud config get-value project)
+TAG=1.0
+
+# Command-line arguments
+OPTS=$(getopt -o p:t: -n ${MYNAME} -- "$@")
+if [ $? != 0 ] ; then
+    logme "ERROR: Failed parsing command line arguments"
+    exit 2
 fi
 
-if [ -n "$2" ]; then
-    TAG="$2"
-else
-    TAG=1.0
-fi
+eval set -- "${OPTS}"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -p) PROJECT="$2"
+        shift 2 ;;
+    -t) TAG="$2"
+        shift 2 ;;
+    *) break ;;
+  esac
+done
 
-# Customizable values
-REGISTRY=gcr.io/${PROJECT}
-
-# Static values
 APP_IMAGE_NAME=provisioner
+REGISTRY=gcr.io/${PROJECT}
 DEPLOYER_IMAGE_TAGGED=${REGISTRY}/${APP_IMAGE_NAME}/deployer:${TAG}
 IMAGE_CLONE_TAGGED=${REGISTRY}/${APP_IMAGE_NAME}/custom_script:${TAG} # Fake image used to run deploy.sh
 
 logme "Building and tagging the deployer image as ${DEPLOYER_IMAGE_TAGGED}"
-exec_cmd docker build -t ${DEPLOYER_IMAGE_TAGGED} --build-arg REGISTRY=${REGISTRY} --build-arg TAG=${TAG} .
+assert_exec_cmd docker build -t ${DEPLOYER_IMAGE_TAGGED} --build-arg REGISTRY=${REGISTRY} --build-arg TAG=${TAG} .
 logme "Pushing ${DEPLOYER_IMAGE_TAGGED}"
-exec_cmd docker push ${DEPLOYER_IMAGE_TAGGED}
+assert_exec_cmd docker push ${DEPLOYER_IMAGE_TAGGED}
 
 logme "Tagging and pushing ${IMAGE_CLONE_TAGGED}"
-exec_cmd docker tag ${DEPLOYER_IMAGE_TAGGED} ${IMAGE_CLONE_TAGGED}
-exec_cmd docker push ${IMAGE_CLONE_TAGGED}
+assert_exec_cmd docker tag ${DEPLOYER_IMAGE_TAGGED} ${IMAGE_CLONE_TAGGED}
+assert_exec_cmd docker push ${IMAGE_CLONE_TAGGED}
 
 logme "Listing available images"
 exec_cmd gcloud container images list --repository=${REGISTRY}/${APP_IMAGE_NAME}
-
-echo "In order to free up the disk space:"
-echo "gcloud container images delete ${DEPLOYER_IMAGE_TAGGED}"
